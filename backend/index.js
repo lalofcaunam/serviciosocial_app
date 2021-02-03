5c17bea51b4b59afc96a3a735c5d51d6e26b85d0
lalofcaunam <lalo.fca.unam@gmail.com>
2021-02-02 18:53:07 -0600

    Configurar el server

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+// Requerimos dotenv para manipular variables globales desde un archivo .env, logger para logs y mongoose para conexion a la BD de Mongo
+require('dotenv').config();
+const logger = require('log4js').getLogger('index');
+const mongoose = require('mongoose');
+
+// Importamos las constantes del archivo index que se encuentra en la carpeta ./api y ./mongo
+const { api, PORT } = require('./api');
+const { MONGO_URI } = require('./config');
+
+// Conexion a la base de datos de Mongo
+mongoose.connect(
+    MONGO_URI,
+    { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false },
+  )
+    .then(() => logger.info('>>>> Base de Datos Mongo, Conectada <<<<'))
+    .catch((err) => logger.error('Error en la conexion de la base de datos: ', err));
+
+// Ponemos a escuchar al servidor en el puerto definido y mostramos en consola un mensaje para conocer esto
+api.listen(PORT, () => logger.info(`Escuchando en el puerto: ${PORT}`));
