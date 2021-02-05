adac1e1eef99c3c141961c06a37c4182613d0707
lalofcaunam <lalo.fca.unam@gmail.com>
2021-02-05 17:26:24 -0600

    Create index.js

---
+++ backend/mongo/index.js
@@ -0,0 +1,21 @@
+const logger = require('log4js').getLogger('index');
+
+const {
+    NODE_ENV,
+    MONGO_DB_NAME,
+    MONGO_HOSTNAME,
+    MONGO_USERNAME,
+    MONGO_PASSWORD
+} = process.env
+
+const config = {
+  PRODUCTION: {
+    MONGO_URI: `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}/${MONGO_DB_NAME}`,
+  },
+  TEST: {
+    MONGO_URI: `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:27017/${MONGO_DB_NAME}`,
+  },
+};
+
+logger.info('NODE_ENV: ', NODE_ENV);
+module.exports = config[NODE_ENV];
