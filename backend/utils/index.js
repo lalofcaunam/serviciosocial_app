29515c2e82fb77d1be890bef04296a66ce9dcefa
lalofcaunam <lalo.fca.unam@gmail.com>
2021-02-05 17:26:09 -0600

    Update index.js

--- backend/utils/index.js
+++ backend/utils/index.js
@@ -2,11 +2,6 @@ const { Codigo } = require('../enum');
 
 module.exports = {
     handler: (doc, res, code) => {
-        if (code == 200){
-            res.status(code).json(doc)
-        }
-        
         res.status(code).json(Object.assign(Codigo[code], doc));
-        
     },
 };
