65f0abd2a5e82a830766d62c1df65214bf3d10df
lalofcaunam <lalo.fca.unam@gmail.com>
2021-02-05 17:25:23 -0600

    Crear modelos

---
+++ backend/models/Licenciatura.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+const licenciaturaSchema = new Schema({
+  // Nombre de la Licenciatura
+  nombre: {
+    type: String,
+    required: true
+  },
+  // Clave de la licenciatura
+  clave: {
+    type: String,
+    required: true
+  }
+});
+
+const Licenciatura = mongoose.model('Licenciatura', licenciaturaSchema);
+
+module.exports = { Licenciatura, licenciaturaSchema };
---
+++ backend/models/index.js
@@ -0,0 +1,11 @@
+const {Asignatura} = require('./Asignatura');
+const {Licenciatura} = require('./Licenciatura');
+const {Semestre} = require('./Semestre');
+const {Config} = require('./Config');
+
+module.exports = {
+    Asignatura,
+    Licenciatura,
+    Semestre,
+    Config,
+};
