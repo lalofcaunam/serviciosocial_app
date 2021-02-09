3c09893bfa80dce6cb108091e221416f85064e32
lalofcaunam <lalo.fca.unam@gmail.com>
2021-02-09 11:56:38 -0600

    Update index.js

--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -1,7 +1,11 @@
 const ConfiguracionController = require('./ConfiguracionController');
 const FCAController = require('./FCAController');
+const AuthController = require('./AuthController');
+const UsuarioController = require('./UsuarioController');
 
 module.exports = {
     ConfiguracionController,
     FCAController,
+    AuthController,
+    UsuarioController,
 };
