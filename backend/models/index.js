376e8dc9d2763eed7875c0d03821c810f6efb2ac
lalofcaunam <lalo.fca.unam@gmail.com>
2021-02-09 11:56:53 -0600

    Update index.js

--- backend/models/index.js
+++ backend/models/index.js
@@ -2,10 +2,14 @@ const {Asignatura} = require('./Asignatura');
 const {Licenciatura} = require('./Licenciatura');
 const {Semestre} = require('./Semestre');
 const {Config} = require('./Config');
+const {Usuario} = require('./Usuario');
+const {Cuestionario} = require('./Cuestionario');
 
 module.exports = {
     Asignatura,
     Licenciatura,
     Semestre,
     Config,
+    Usuario,
+    Cuestionario,
 };
