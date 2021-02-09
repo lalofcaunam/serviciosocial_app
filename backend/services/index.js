839420a14309cd9e0377825b31c3c9dac4279d68
lalofcaunam <lalo.fca.unam@gmail.com>
2021-02-09 11:58:03 -0600

    Update index.js

--- backend/services/index.js
+++ backend/services/index.js
@@ -2,10 +2,14 @@ const ConfiguracionService = require('./ConfiguracionService');
 const LicenciaturaService = require('./LicenciaturaService');
 const SemestreService = require('./SemestreService');
 const AsignaturaService = require('./AsignaturaService');
+const UsuarioService = require('./UsuarioService');
+const CuestionarioService = require('./CuestionarioService');
 
 module.exports = {
     ConfiguracionService,
     LicenciaturaService,
     SemestreService,
     AsignaturaService,
+    UsuarioService,
+    CuestionarioService,
 };
