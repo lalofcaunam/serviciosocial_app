7566bcbffbd9e21d3983ec8d0f54d6004cafee03
lalofcaunam <lalo.fca.unam@gmail.com>
2021-02-09 11:56:46 -0600

    Update index.js

--- backend/dtos/index.js
+++ backend/dtos/index.js
@@ -1,7 +1,11 @@
-const ConfiguracionDTO = require('./ConfiguracionDTO');
+const ConfiguracionDto = require('./ConfiguracionDto');
 const FcaDto = require('./FcaDto');
+const UsuarioDto = require('./UsuarioDto');
+const CuestionarioDto = require('./CuestionarioDto');
 
 module.exports = {
-    ConfiguracionDTO,
+    ConfiguracionDto,
     FcaDto,
+    UsuarioDto,
+    CuestionarioDto,
 };
