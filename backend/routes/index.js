0452c3370f5e67ee7ab8056e380bc6b979c17971
lalofcaunam <lalo.fca.unam@gmail.com>
2021-02-10 18:48:21 -0600

    Update index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,5 +6,6 @@ router.use(require('./Configuracion.routes'));
 router.use(require('./FCA.routes'));
 router.use(require('./Auth.routes'));
 router.use(require('./Usuario.routes'));
+router.use(require('./Cuestionario.routes'));
 
 module.exports = router;
