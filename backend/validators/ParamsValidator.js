3346f5abcccda823fdfa125bc0ed982c8b20602d
lalofcaunam <lalo.fca.unam@gmail.com>
2021-02-13 14:43:11 -0600

    Crear validador de parametros
    Sigue en desarrollo

---
+++ backend/validators/ParamsValidator.js
@@ -0,0 +1,5 @@
+// Aqui se validara que los parametros esperados exitan
+
+// 1. Recibir los params, el nombre del param esperado y el origen
+// 2. Si existe, continua
+// 3. Si no existe, se contesta desde aqui el error
