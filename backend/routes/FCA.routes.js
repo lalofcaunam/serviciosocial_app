065e98d26db36b1b0fd12285a98434d3fd5ab344
lalofcaunam <lalo.fca.unam@gmail.com>
2021-02-16 14:05:35 -0600

    Update FCA.routes.js

--- backend/routes/FCA.routes.js
+++ backend/routes/FCA.routes.js
@@ -3,9 +3,17 @@ const express = require('express');
 const router = express.Router();
 
 const { FCAController } = require('../controllers');
-const { TokenMiddleware, HeaderMiddleware } = require('../middlewares');
+const { TokenMiddleware } = require('../middlewares');
 
-router.get('/fca?', TokenMiddleware.verifyToken, HeaderMiddleware.validarProfesor, FCAController.leerTodasLicenciaturasOSemestres);
-router.get('/fca/asignaturas?', TokenMiddleware.verifyToken, HeaderMiddleware.validarProfesor, FCAController.leerTodasAsignaturasFiltro);
+router.get(
+    '/fca?', 
+    TokenMiddleware.verifyToken, 
+    FCAController.leerTodasLicenciaturasOSemestres
+);
+router.get(
+    '/fca/asignaturas?', 
+    TokenMiddleware.verifyToken,
+    FCAController.leerTodasAsignaturasFiltro
+);
 
 module.exports = router;
