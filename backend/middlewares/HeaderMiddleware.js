a320b67fc9bf8eeff82960ab0f0483a9464e557a
lalofcaunam <lalo.fca.unam@gmail.com>
2021-02-16 14:05:04 -0600

    Update HeaderMiddleware.js

--- backend/middlewares/HeaderMiddleware.js
+++ backend/middlewares/HeaderMiddleware.js
@@ -3,103 +3,45 @@ const { handler } = require('../utils');
 const { Message } = require('../enum');
 const { UsuarioService } = require('../services');
 
-const usuarioExiste = async (idusuario) => {
-  try {
-    // Validar que el usuario existe
-    logger.debug('HeaderMiddleware - usuarioExiste: Mandar a llamar al servicio leerUno de Usuario');
-    const usuarioEncontrado = await UsuarioService.leerUno(idusuario);
+module.exports = {
+  validar: async (validar, res, req) => {
+    try {
 
-    // Si no existe terminar el proceso
-    if(usuarioEncontrado == null) {
+      // Validar que el header no venga vacio
+      if(req.headers.idusuario == null || req.headers.idusuario == undefined){
+        logger.error('No viene el header idUsuario');
+        return handler(Message('No viene el header idUsuario', 400), res, 400);
+      }
+
+      // Validar que el usuario exista
+      logger.debug('HeaderMiddleware - usuarioExiste: Mandar a llamar al servicio leerUno de Usuario');
+      const usuarioEncontrado = await UsuarioService.leerUno(req.headers.idusuario);
+
+      // Si no existe terminar el proceso
+      if(!usuarioEncontrado) {
         logger.debug('El usuario no existe');
         logger.info('<< Termina middleware usuarioExiste');
         return handler(Message('El usuario no existe', 404), res, 404);
-    }
-
-    // Si regresa false, significa que hubo un error en el servicio
-    if(!usuarioEncontrado) {
-        logger.debug('Hubo un error en el servicio leerUno');
-        logger.info('<< Termina middleware usuarioExiste');
-        return handler(Message('Hubo un error en el servicio leerUno', 500), res, 500);
-    }
-
-    return usuarioEncontrado;
-
-  } catch (error) {
-      logger.error('Error en middleware usuarioExiste: ', error);
-      return handler(Message('Error en middleware usuarioExiste', 500), res, 500);
-  }
-};
-
-const validarHeaderIdUsuario = async (headers) => {
-  const { idusuario } = headers;
-
-  // Validar que venga el header idusuario
-  if(idusuario == null || idusuario == undefined){
-    logger.error('No viene el Header idusuario, valor: ', idusuario);
-    return handler(Message('No viene el Header idusuario', 400), res, 400);
-  }
-
-  // Validar que el usuario exista
-  const usuarioEncontrado = await usuarioExiste(idusuario);
-
-  return usuarioEncontrado;
-}
-
-module.exports = {
-  validarProfesor: async (req, res, next) => {
-    try {
-
-      // Validar que el usuario exista
-      const usuarioEncontrado = await validarHeaderIdUsuario(req.headers);
-
-      // Validar el rol del usuario
-      if(usuarioEncontrado.rol != 'Profesor'){
-        logger.error('El usuario no tiene el rol de Profesor');
-        return handler(Message('El usuario no tiene el rol de Profesor', 401), res, 401);
       }
 
-      next();
+      // Si regresa false, significa que hubo un error en el servicio
+      if(usuarioEncontrado == 'Error') {
+          logger.debug('Hubo un error en el servicio leerUno');
+          logger.info('<< Termina middleware usuarioExiste');
+          return handler(Message('Hubo un error en el servicio leerUno', 500), res, 500);
+      }
+
+      // Validar el rol del usuario
+      if(usuarioEncontrado.rol != validar){
+        logger.error(`El usuario no tiene el rol de ${validar}`);
+        return handler(Message(`El usuario no tiene el rol de ${validar}`, 401), res, 401);
+      }
+
+      return true;
 
    } catch (error) {
      logger.error('Error en middleware validarProfesor: ', error);
      return handler(Message('Error en middleware validarProfesor', 500), res, 500);
-   }
-  },
-  validarAlumno: async (req, res, next) => {
-    try {
-      // Validar que el usuario exista
-      const usuarioEncontrado = await validarHeaderIdUsuario(req.headers);
-
-      // Validar el rol del usuario
-      if(usuarioEncontrado.rol != 'Alumno'){
-        logger.error('El usuario no tiene el rol de Alumno');
-        return handler(Message('El usuario no tiene el rol de Alumno', 401), res, 401);
-      }
-
-      next();
-
-   } catch (error) {
-     logger.error('Error en middleware validarAlumno: ', error);
-     return handler(Message('Error en middleware validarAlumno', 500), res, 500);
-   }
-  },
-  validarUsuario: async (req, res, next) => {
-    try {
-      // Validar que el usuario exista
-      const usuarioEncontrado = await validarHeaderIdUsuario(req.headers);
-
-      // Validar el rol del usuario
-      if(usuarioEncontrado.rol != null){
-        logger.error('El usuario no tiene un rol');
-        return handler(Message('El usuario no tiene un rol', 401), res, 401);
-      }
-
-      next();
-
-   } catch (error) {
-     logger.error('Error en middleware validarUsuario: ', error);
-     return handler(Message('Error en middleware validarUsuario', 500), res, 500);
    }
   }
 };
