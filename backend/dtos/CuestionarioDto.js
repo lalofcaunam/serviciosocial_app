11623d7b480756412bd1ea0bf5ab72338eb61243
lalofcaunam <lalo.fca.unam@gmail.com>
2021-02-20 13:19:18 -0600

    Agregar log de error

--- backend/dtos/CuestionarioDto.js
+++ backend/dtos/CuestionarioDto.js
@@ -1,23 +1,42 @@
+const logger = require('log4js').getLogger('CuestionarioDto');
 const { Cuestionario } = require('../models');
 
 module.exports = {
     
     // Crear un cuestionario
-    crearUno: (body) => new Cuestionario(body).save(),
+    crearUno: (body) => new Cuestionario(body).save().catch((err) => {
+        logger.error(err);
+        return 'Error'
+    }),
 
     // Leer un cuestionario
-    leerUno: (filtro) => Cuestionario.findOne(filtro).catch(() => 'Error'),
+    leerUno: (filtro) => Cuestionario.findOne(filtro).catch((err) => {
+        logger.error(err);
+        return 'Error'
+    }),
 
     // Leer todas los cuestionarios
-    leerTodos: () => Usuario.find().catch(() => 'Error'),
+    leerTodos: () => Usuario.find().catch((err) => {
+        logger.error(err);
+        return 'Error'
+    }),
 
     // Leer todas los cuestionarios
-    leerTodosConFiltro: (filtro) => Cuestionario.find(filtro).catch(() => 'Error'),
+    leerTodosConFiltro: (filtro) => Cuestionario.find(filtro).catch((err) => {
+        logger.error(err);
+        return 'Error'
+    }),
 
     // Actualizar un cuestionario
-    updateOne: (idCuestionario, body) => Cuestionario.findByIdAndUpdate({_id: idCuestionario}, body, { new: true }).catch(() => 'Error'),
+    updateOne: (idCuestionario, body) => Cuestionario.findByIdAndUpdate({_id: idCuestionario}, body, { new: true }).catch((err) => {
+        logger.error(err);
+        return 'Error'
+    }),
 
     // Borrar un cuestionario
-    deleteOne: (idCuestionario) => Cuestionario.findByIdAndRemove({_id: idCuestionario}).catch(() => 'Error'),
+    deleteOne: (idCuestionario) => Cuestionario.findByIdAndRemove({_id: idCuestionario}).catch((err) => {
+        logger.error(err);
+        return 'Error'
+    }),
 
 }
