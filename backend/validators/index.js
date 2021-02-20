3a8d6df4f1c7668d90885d7d4225840a6984ae1c
lalofcaunam <lalo.fca.unam@gmail.com>
2021-02-20 13:58:41 -0600

    Eliminar BusquedaValidator

--- backend/validators/index.js
+++ backend/validators/index.js
@@ -2,10 +2,12 @@ const GeneralValidator = require('./GeneralValidator');
 const FCAValidator = require('./FCAValidator');
 const AuthValidator = require('./AuthValidator');
 const HeaderValidator = require('./HeaderValidator');
+const BusquedaValidator = require('./BusquedaValidator');
 
 module.exports = {
     GeneralValidator,
     FCAValidator,
     AuthValidator,
     HeaderValidator,
+    BusquedaValidator,
 };
