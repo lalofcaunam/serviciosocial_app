31311e1e8577cf7b9e0b572917ec60a7379860a0
lalofcaunam <lalo.fca.unam@gmail.com>
2021-02-20 13:17:49 -0600

    Cambiar return

--- backend/validators/BusquedaValidator.js
+++ backend/validators/BusquedaValidator.js
@@ -1,6 +1,4 @@
 const logger = require('log4js').getLogger('BusquedaUtil');
-const { handler } = require('../utils');
-const { Message } = require('../enum');
 const { 
     LicenciaturaService,
     SemestreService,
@@ -38,31 +36,28 @@ const validarModelo = async (modelo, id) => {
 }
 
 module.exports = {
-    buscarUno: async (modelo, id, origen) => {
+    buscarUno: async (modelo, id) => {
         try {
 
             // llamar a la funcion validarModelo, (esta consultara dependiendo el tipo de modelo) y regresara el objeto, false o 'Error'
             const consulta = await validarModelo(modelo, id);
 
+            // Validar que exista el valor mandado a buscar
+            if ( consulta === false){
+                logger.debug(`No existe ningun elemento de ${modelo}`);
+                return { error: true, message: `No existe ningun elemento de ${modelo}`, code: 204 };
+            }
+
             // Validar que se realizo alguna operación
-            if ( consulta == 0){
+            if ( consulta === 0){
                 logger.debug(`No existe el modelo ${modelo}`);
-                logger.info(`<< Termina controller ${origen}`);
-                return handler(Message(`No existe el modelo ${modelo}`, 500), res, 500);
+                return { error: true, message: `No existe el modelo ${modelo}`, code: 500 };
             }
 
             // Validar que no hubo errores en la consulta
-            if ( consulta == 'Error'){
+            if ( consulta === 'Error'){
                 logger.debug(`Hubo un error al consultar el servicio leerUno de ${modelo}`);
-                logger.info(`<< Termina controller ${origen}`);
-                return handler(Message(`Hubo un error al consultar el servicio leerUno de ${modelo}`, 500), res, 500);
-            }
-
-            // Validar que exista el valor mandado a buscar
-            if ( consulta == null){
-                logger.debug(`No existe ningun elemento de ${modelo}`);
-                logger.info(`<< Termina controller ${origen}`);
-                return handler(Message(`No existe ningun elemento de ${modelo}`, 204), res, 204);
+                return { error: true, message: `Hubo un error al consultar el servicio leerUno de ${modelo}`, code: 500 };
             }
 
             logger.debug(`Objeto encontrado ${consulta}`);
@@ -71,8 +66,7 @@ module.exports = {
         } catch (error) {
             // Si existe un error , devolver el error
             logger.error('Error en la utilidad buscarUno: ', error);
-            logger.info(`<< Termina controller ${origen}`);
-            return handler(Message('Ocurrio un error en la utilidad buscarUno', 500), res, 500);
+            return { error: true, message: 'Ocurrio un error en la utilidad buscarUno', code: 500 };
         }
     },
 };
