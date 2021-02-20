39020676cef25a7eea6a9032cd35e80aa0500814
lalofcaunam <lalo.fca.unam@gmail.com>
2021-02-20 13:52:00 -0600

    Agregar log de error

--- backend/dtos/FcaDto.js
+++ backend/dtos/FcaDto.js
@@ -1,15 +1,30 @@
+const logger = require('log4js').getLogger('FcaDto');
+
 module.exports = {
     
     // Crear un user
-    creacionMasiva: (body, Modelo) => Modelo.insertMany(body).catch(() => 'Error'),
+    creacionMasiva: (body, Modelo) => Modelo.insertMany(body).catch((err) => {
+        logger.error(err);
+        return 'Error'
+    }),
 
     // Leer todos los archivos de una compañia y de un folder
-    leerUno: (clave, Modelo) => Modelo.findOne(clave).catch(() => 'Error'),
+    leerUno: (clave, Modelo) => Modelo.findOne(clave)
+        .catch((err) => {
+        logger.error(err);
+        return 'Error'
+    }),
 
     // Leer un archivo
-    leerTodos: (Modelo) => Modelo.find().catch(() => 'Error'),
+    leerTodos: (Modelo) => Modelo.find().catch((err) => {
+        logger.error(err);
+        return 'Error'
+    }),
 
     // Leer un archivo
-    leerMuchosPorFiltro: (filtro, Modelo) => Modelo.find(filtro).catch(() => 'Error'),
+    leerMuchosPorFiltro: (filtro, Modelo) => Modelo.find(filtro).catch((err) => {
+        logger.error(err);
+        return 'Error'
+    }),
 
 }
