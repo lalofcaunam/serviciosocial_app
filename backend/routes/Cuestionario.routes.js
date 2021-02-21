cf21cda9974e2793d10f34328717be7a74211e08
lalofcaunam <lalo.fca.unam@gmail.com>
2021-02-20 21:05:04 -0600

    Agregar rutas para leer un cuestionario y leer todos los cuestionarios

--- backend/routes/Cuestionario.routes.js
+++ backend/routes/Cuestionario.routes.js
@@ -4,11 +4,29 @@ const router = express.Router();
 
 const { CuestionarioController } = require('../controllers');
 const { TokenMiddleware } = require('../middlewares');
+const { HeaderValidator, CuestionarioValidator } = require('../validators');
 
 router.post(
-    '/cuestionarios', 
-    TokenMiddleware.verifyToken, 
-    CuestionarioController.crearUno
+    '/cuestionarios',
+    HeaderValidator.general,
+    CuestionarioValidator.crearUno,
+    TokenMiddleware.verifyToken,
+    CuestionarioController.crearUno,
 );
+
+router.get(
+    '/cuestionarios',
+    HeaderValidator.general,
+    TokenMiddleware.verifyToken,
+    CuestionarioController.leerTodos,
+);
+
+router.get(
+    '/cuestionarios/:idCuestionario',
+    HeaderValidator.general,
+    TokenMiddleware.verifyToken,
+    CuestionarioController.leerUno,
+);
+
 
 module.exports = router;
