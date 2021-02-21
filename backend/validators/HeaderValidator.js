846616c8390eb293a732a41d765dfd20bcc0fdea
lalofcaunam <lalo.fca.unam@gmail.com>
2021-02-20 21:07:43 -0600

    Agregar validacion de header idUsuario

--- backend/validators/HeaderValidator.js
+++ backend/validators/HeaderValidator.js
@@ -1,4 +1,7 @@
+const logger = require('log4js').getLogger('HeaderValidator');
 const { celebrate, Joi, Segments } = require('celebrate');
+const { esNuloIndefinido } = require('../utils');
+const { buscarUno } = require('./BusquedaValidator');
 
 module.exports = {
     general: celebrate({
@@ -10,4 +13,35 @@ module.exports = {
             'content-length': Joi.string(),
         }),
     }),
+    idUsuario: async (rol, req) => {
+        try {
+
+            // Validar que no venga nulo el header
+            const headerNuloIndefinido = esNuloIndefinido([req.headers.idusuario], ['HeaderIdUsuario']);
+            if(headerNuloIndefinido.error){
+                return { error: headerNuloIndefinido.error, message: headerNuloIndefinido.message, code: headerNuloIndefinido.code };
+            }
+
+            // Validar que exista el usuario
+            const usuarioEncontrado = await buscarUno('Usuario', req.headers.idusuario);
+            if(usuarioEncontrado.error){
+                return { error: usuarioEncontrado.error, message: usuarioEncontrado.message, code: 400 };
+            }
+
+            // Si el rol es diferente de nulo
+            if(rol != null){
+                // Validar el rol del usuario
+                if(usuarioEncontrado.rol !== rol){
+                    logger.error(`El usuario no tiene el rol de ${rol}`);
+                    return { error: true, message: `El usuario no tiene el rol de ${rol}`, code: 401 };
+                }
+            }
+
+            return usuarioEncontrado;
+
+        } catch (error) {
+            logger.error('Error en validador idUsuario: ', error);
+            return { error: true, message: `Error en validador idUsuario`, code: 500 };
+        }
+    },
 }
