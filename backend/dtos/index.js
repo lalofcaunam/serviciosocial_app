874a57593059697b5f30bb66b21948a524d09494
lalofcaunam <lalo.fca.unam@gmail.com>
2021-02-24 20:48:15 -0600

    Agregar dto de Pregunta y Respuesta

--- backend/dtos/index.js
+++ backend/dtos/index.js
@@ -2,10 +2,14 @@ const ConfiguracionDto = require('./ConfiguracionDto');
 const FcaDto = require('./FcaDto');
 const UsuarioDto = require('./UsuarioDto');
 const CuestionarioDto = require('./CuestionarioDto');
+const PreguntaDto = require('./PreguntaDto');
+const RespuestaDto = require('./RespuestaDto');
 
 module.exports = {
     ConfiguracionDto,
     FcaDto,
     UsuarioDto,
     CuestionarioDto,
+    PreguntaDto,
+    RespuestaDto,
 };
