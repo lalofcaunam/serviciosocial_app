054c3a892cf1656d0638f63dcd38efc387ef189e
lalofcaunam <lalo.fca.unam@gmail.com>
2021-02-24 21:00:13 -0600

    Crear rutas de pregunta

---
+++ backend/routes/Pregunta.routes.js
@@ -0,0 +1,40 @@
+const express = require('express');
+
+const router = express.Router();
+
+const { CuestionarioController } = require('../controllers');
+const { TokenMiddleware } = require('../middlewares');
+const { HeaderValidator, CuestionarioValidator } = require('../validators');
+
+router.post(
+    '/cuestionarios',
+    HeaderValidator.general,
+    CuestionarioValidator.crearUno,
+    TokenMiddleware.verifyToken,
+    CuestionarioController.crearUno,
+);
+
+router.get(
+    '/cuestionarios',
+    HeaderValidator.general,
+    TokenMiddleware.verifyToken,
+    CuestionarioController.leerTodos,
+);
+
+router.get(
+    '/cuestionarios/:idCuestionario',
+    HeaderValidator.general,
+    TokenMiddleware.verifyToken,
+    CuestionarioController.leerUno,
+);
+
+router.put(
+    '/cuestionarios/:idCuestionario',
+    HeaderValidator.general,
+    CuestionarioValidator.actualizarUno,
+    TokenMiddleware.verifyToken,
+    CuestionarioController.actualizarUno,
+);
+
+
+module.exports = router;
