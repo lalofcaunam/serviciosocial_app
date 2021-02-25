728235a884f3d49aa49255387b4a3512f697535e
lalofcaunam <lalo.fca.unam@gmail.com>
2021-02-24 20:41:21 -0600

    Agregar controladores para recuperación de contraseña

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -1,7 +1,12 @@
 const logger = require('log4js').getLogger('AuthController');
-const { handler, enviarCorreo, compararContrasenias, crearToken } = require('../utils');
+const { handler, enviarCorreo, compararContrasenias, crearToken, esNuloIndefinido, crearUrl } = require('../utils');
 const { Message } = require('../enum');
+const jwt = require('jsonwebtoken');
+const moment = require('moment');
+const bcrypt = require('bcrypt');
+const SALT_WORK_FACTOR = 10;
 const { UsuarioService } = require('../services');
+const url = crearUrl();
 
 module.exports = {
 
@@ -39,7 +44,16 @@ module.exports = {
 
             // Mandar correo de validacion de email
             logger.debug('AuthController - signup: Mandar a llamar a la utilidad de envio el correo de verificacion');
-            const envioCorreo = await enviarCorreo(usuarioCreado);
+            const envioCorreo = await enviarCorreo({
+                to: usuarioCreado.correo,
+                from: process.env.SENDER_SENDGRID, // email de rfl
+                templateId: process.env.TEMPLATE_VERIFIEDEMAIL_SENDGRID,
+                dynamic_template_data: {
+                    nombreUsuario: usuarioEncontrado.nombre, ///usuarios/:token/verificar
+                    urlVerificacion: `${url}/usuarios/${token}/verificar`,
+                    expiracion: moment().add(20, 'minutes').format('LLL'),
+                }
+            });
 
             // Si regresa false, significa que hubo un error en la utilidad
             if(!envioCorreo) {
@@ -137,6 +151,269 @@ module.exports = {
 
         }
 
+    },
+
+    // Enviar la vista 'enviar-email-resetpass'
+    pantallaCorreoReset: (req, res) => {
+        res.render('enviar-email-resetpass', {
+            titulo: 'enviar-email-resetpass',
+            endpoint:`${url}/usuarios/reset`
+        });
+    },
+
+    // Enviar correo para cambio de contraseña
+    enviarCorreoReset: async (req, res) => {
+        try {
+            logger.info('>> Inicia controller enviarCorreoReset');
+            logger.debug('Req Body: ', req.body);
+
+            // Buscar al usuario por correo
+            logger.debug('AuthController - signup: Mandar a llamar al servicio leerUnoPorEmail de Usuario');
+            const usuarioEncontrado = await UsuarioService.leerUnoPorEmail(req.body.correo, true);
+
+            // Si regresa false, significa que el usuario no existe
+            if(!usuarioEncontrado) {
+                logger.debug('AuthController - enviarCorreoReset: El usuario no existe');
+                logger.info('<< Termina controller enviarCorreoReset');
+                return res.render('error', {
+                    titulo: 'Ups',
+                    error: 'Este usuario no existe'
+                });
+            }
+
+            // Si regresa 'Error', significa que hubo un error en el servicio
+            if(usuarioEncontrado === 'Error') {
+                logger.debug('AuthController - enviarCorreoReset: Hubo un error en el servicio leerUnoPorEmail');
+                logger.info('<< Termina controller signup');
+                return res.render('error', {
+                    titulo: 'Ups',
+                    error: 'Ocurrio un error inesperado en el servicio'
+                });
+            }
+
+            // Generar token para link del correo
+            const token = crearToken({
+                idUsuario: usuarioEncontrado._id,
+                correo: usuarioEncontrado.correo
+            }, '20min', usuarioEncontrado.contrasenia);
+
+            // Mandar correo para cambiar la contraseña
+            logger.debug('AuthController - enviarCorreoReset: Mandar a llamar a la utilidad de envio de correo');
+            const envioCorreo = await enviarCorreo({
+                to: usuarioEncontrado.correo,
+                from: process.env.SENDER_SENDGRID,
+                templateId: process.env.TEMPLATE_CHANGEPASS_SENDGRID,
+                dynamic_template_data: {
+                    nombreUsuario: usuarioEncontrado.nombre,
+                    urlCambioContrasenia: `${url}/usuarios/reset/${token}`,
+                    expiracion: moment().add(20, 'minutes').format('LLL'),
+                }
+            });
+
+            // Si regresa false, significa que hubo un error en la utilidad
+            if(!envioCorreo) {
+                logger.debug('Hubo un error en la utilidad enviarCorreo');
+                logger.info('<< Termina controller signup');
+                return res.render('error', {
+                    titulo: 'Ups',
+                    error: 'Ocurrio un error inesperado en el servicio'
+                });
+            }
+
+            logger.info('<< Termina controller enviarCorreoReset');
+            return res.render('respuesta-sicorreoexiste-resetpass', {
+                titulo: 'Si el correo existe'
+            });
+
+        } catch (error) {
+
+            // Si existe un error en el envio, devolver el error
+            logger.error(error);
+            return res.render('error', {
+                titulo: 'Ups',
+                error: 'Ocurrio un error inesperado en el servicio'
+            });
+
+        }
+    },
+
+    // Enviar pantalla para cambio de contraseña
+    enviarPantallaReset: async (req, res) => {
+        try {
+            logger.info('>> Inicia controller enviarPantallaReset');
+            logger.debug('Req Params: ', req.params);
+
+            // Validar que no venga nulo el parametro token
+            const tokenNuloIndefinido = esNuloIndefinido([req.params.token], ['ParamToken']);
+            if(tokenNuloIndefinido.error){
+                logger.debug('AuthController - enviarPantallaReset: Hubo un error en la utilidad esNuloIndefinido');
+                logger.info('<< Termina controller enviarPantallaReset');
+                return res.render('error', {
+                    titulo: 'Ups',
+                    error: 'Ocurrio un error inesperado en el servicio'
+                });
+            }
+
+            // Buscar al usuario por correo
+            logger.debug('AuthController - enviarPantallaReset: Mandar a llamar al servicio leerUnoPorEmail de Usuario');
+            const usuarioEncontrado = await UsuarioService.leerUnoPorEmail(jwt.decode(req.params.token).correo, true);
+
+            // Si regresa false, significa que el usuario no existe
+            if(!usuarioEncontrado) {
+                logger.debug('AuthController - enviarPantallaReset: El usuario no existe');
+                logger.info('<< Termina controller signup');
+                return res.render('error', {
+                    titulo: 'Ups',
+                    error: 'Este usuario no existe'
+                });
+            }
+
+            // Si regresa 'Error', significa que hubo un error en el servicio
+            if(usuarioEncontrado === 'Error') {
+                logger.debug('AuthController - enviarPantallaReset: Hubo un error en el servicio leerUnoPorEmail');
+                logger.info('<< Termina controller enviarPantallaReset');
+                return res.render('error', {
+                    titulo: 'Ups',
+                    error: 'Ocurrio un error inesperado en el servicio'
+                });
+            }
+
+            let decode = null;
+
+            // Decodificar el token con la llave
+            jwt.verify(req.params.token, usuarioEncontrado.contrasenia, function (err, decoded){
+                if(err){
+                    console.log(err)
+                    if(err.name === 'TokenExpiredError'){
+                        logger.debug('El token a expirado');
+                        logger.info('<< Termina controller enviarPantallaReset');
+                        return decode = {
+                            error: true,
+                            type: err.name,
+                            message: `El token expiro el día ${moment(err.expiredAt).format('LLL')}`,
+                            code: 500
+                        }
+                    } else {
+                        logger.debug('El token es invalido');
+                        logger.info('<< Termina controller enviarPantallaReset');
+                        return decode = {
+                            error: true,
+                            type: err.name,
+                            message: `El token es invalido`,
+                            code: 500
+                        }
+                    }
+                } else decode = decoded;
+            });
+
+            if(decode.error){
+                // Si el error es por expiracion del token, mandar pantalla para reenviar correo
+                if(decode.type === 'TokenExpiredError'){
+                    return res.render('tokenexp-resetpass', {
+                        titulo: 'Token Expirado',
+                        expiracion: decode.message,
+                        correo: usuarioEncontrado.correo,
+                        endpoint:`${url}/usuarios/reset`
+                    });
+                }
+                // Si el error es por que el token es invalido, mandar pantalla de error
+                else{
+                    return res.render('error', {
+                        titulo: 'Ups',
+                        error: 'Ocurrio un error inesperado en el servicio'
+                    });
+                }
+            }
+
+            logger.info('<< Termina controller enviarPantallaReset');
+            // Enviar la pantalla para el cambio de contraseña
+            return res.render('form-resetpass', {
+                titulo: 'Cambio de contraseña',
+                nombre: usuarioEncontrado.nombre,
+                endpoint:`${url}/usuarios/reset/${usuarioEncontrado.id}`
+            });
+
+        } catch (error) {
+
+            // Si existe un error en el envio de la pantalla, devolver el error
+            logger.error(error);
+            return res.render('error', {
+                titulo: 'Ups',
+                error: 'Ocurrio un error inesperado en el servicio'
+            });
+
+        }
+    },
+
+    // Cambiar contraseña del usuario
+    reset: async (req, res) => {
+
+        try {
+            logger.info('>> Inicia controller reset');
+            logger.debug('Req Body: ', req.body);
+            logger.debug('Req Params: ', req.params);
+
+            // TODO: validar que los params no vengan nulos "idUsuario"
+
+            // Encriptar contraseña del usuario
+            const hashPassword = bcrypt.hashSync(req.body.contrasenia, SALT_WORK_FACTOR);
+
+            // Actualizar la contraseña del usuario
+            const usuarioActualizado = await UsuarioService.actualizarUno({_id: req.params.idUsuario, body: {contrasenia: hashPassword} });
+
+            // Si regresa false, significa que el usuario no existe
+            if(!usuarioActualizado) {
+                logger.debug('AuthController - reset: El usuario no existe');
+                logger.info('<< Termina controller reset');
+                return res.render('error', {
+                    titulo: 'Ups',
+                    error: 'Este usuario no existe'
+                });
+            }
+
+            // Si regresa 'Error', significa que hubo un error en el servicio
+            if(usuarioActualizado === 'Error') {
+                logger.debug('AuthController - reset: Hubo un error en el servicio actualizarUno');
+                logger.info('<< Termina controller reset');
+                return res.render('error', {
+                    titulo: 'Ups',
+                    error: 'Ocurrio un error inesperado en el servicio'
+                });
+            }
+
+            // Mandar correo para cambiar la contraseña
+            logger.debug('AuthController - reset: Mandar a llamar a la utilidad de envio de correo');
+            const envioCorreo = await enviarCorreo({
+                to: usuarioActualizado.correo,
+                from: process.env.SENDER_SENDGRID,
+                templateId: process.env.TEMPLATE_SUCCESSCHANGEPASS_SENDGRID,
+                dynamic_template_data: {
+                    nombreUsuario: usuarioActualizado.nombre
+                }
+            });
+
+            // Si regresa false, significa que hubo un error en la utilidad
+            if(!envioCorreo) {
+                logger.debug('AuthController - reset: Hubo un error en la utilidad enviarCorreo');
+                logger.info('<< Termina controller reset');
+            }
+
+            logger.info('<< Termina Controller reset');
+            res.render('success-resetpass', {
+                titulo: 'Exitoso',
+                nombre: usuarioActualizado.nombre
+            });
+
+        } catch (error) {
+
+            // Si existe un error en la actualización, devolver el error
+            logger.error(error);
+            return res.render('error', {
+                titulo: 'Ups',
+                error: 'Ocurrio un error inesperado en el servicio'
+            });
+
+        }
     },
 
 };
