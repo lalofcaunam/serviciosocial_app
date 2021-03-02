61951cba68f3a718938a70d377263073d4caf215
lalofcaunam <lalo.fca.unam@gmail.com>
2021-03-01 20:14:22 -0600

    Renombrar archivo

--- backend/validators/PreguntaValidator.js
+++ backend/validators/PreguntaValidator.js
@@ -4,11 +4,21 @@ module.exports = {
     crearUno: celebrate({
         [Segments.BODY]: Joi.object().keys({
             texto: Joi.string().required(),
-            idCuestionario: Joi.string().required(),
             respuestas: Joi.array().items(Joi.object().keys({
                 texto: Joi.string().required(),
                 esCorrecta: Joi.boolean().required(),
                 comentario: Joi.string().required(),
+            })).min(4).max(4),
+        }),
+    }),
+    actualizarUno: celebrate({
+        [Segments.BODY]: Joi.object().keys({
+            texto: Joi.string(),
+            respuestas: Joi.array().items(Joi.object().keys({
+                _id: Joi.string(),
+                texto: Joi.string(),
+                esCorrecta: Joi.boolean(),
+                comentario: Joi.string(),
             })).min(4).max(4),
         }),
     }),
