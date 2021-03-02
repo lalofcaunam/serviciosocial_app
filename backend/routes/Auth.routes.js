8de68915163d76da0dad0d815321e20ab1cb089e
lalofcaunam <lalo.fca.unam@gmail.com>
2021-03-01 20:14:43 -0600

    Eliminar ruta

--- backend/routes/Auth.routes.js
+++ backend/routes/Auth.routes.js
@@ -7,7 +7,6 @@ const { AuthValidator } = require('../validators');
 
 router.post('/usuarios/signup', AuthValidator.signup, AuthController.signup);
 router.post('/usuarios/login', AuthValidator.login, AuthController.login);
-router.get('/usuarios/reset', AuthController.pantallaCorreoReset);
 router.post('/usuarios/reset?', AuthValidator.enviarCorreoReset, AuthController.enviarCorreoReset);
 router.post('/usuarios/reset/:idUsuario', AuthValidator.reset, AuthController.reset);
 router.get('/usuarios/reset/:token', AuthController.enviarPantallaReset);
