8f1dcbbc46648ac91422cdfa3dd1a8921edee45f
lalofcaunam <lalo.fca.unam@gmail.com>
2021-03-10 20:30:23 -0600

    Agregar dto de Historial

--- backend/dtos/index.js
+++ backend/dtos/index.js
@@ -3,6 +3,7 @@ const FcaDto = require('./FcaDto');
 const UsuarioDto = require('./UsuarioDto');
 const CuestionarioDto = require('./CuestionarioDto');
 const PreguntaDto = require('./PreguntaDto');
+const HistorialDto = require('./HistorialDto');
 
 module.exports = {
     ConfiguracionDto,
@@ -10,4 +11,5 @@ module.exports = {
     UsuarioDto,
     CuestionarioDto,
     PreguntaDto,
+    HistorialDto,
 };
