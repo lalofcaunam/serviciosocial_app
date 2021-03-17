92e9b348ed70119b84d8c5553e152d026b26c52c
lalofcaunam <lalo.fca.unam@gmail.com>
2021-03-17 00:03:37 -0600

    iconoApp, imagenSplash, label correo, inicio de sesion prueba

--- front/EstudiaFCA/app/src/main/java/com/lalofcaunam/estudiafca/Login/LoginActivity.java
+++ front/EstudiaFCA/app/src/main/java/com/lalofcaunam/estudiafca/Login/LoginActivity.java
@@ -6,23 +6,38 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.auth0.android.jwt.JWT;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.lalofcaunam.estudiafca.API.JsonApi;
+import com.lalofcaunam.estudiafca.Alumno.CuestionariosAlumno;
 import com.lalofcaunam.estudiafca.Modelos.Login;
 import com.lalofcaunam.estudiafca.Modelos.ResultResponse;
+import com.lalofcaunam.estudiafca.Profesor.CuestionariosProfesor;
 import com.lalofcaunam.estudiafca.R;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
 
+import okhttp3.MediaType;
+import okhttp3.ResponseBody;
+import okio.BufferedSource;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -71,29 +86,45 @@ public class LoginActivity extends AppCompatActivity {
                 correo = editTextCorreo.getText().toString().trim();
                 password = editTextPassword.getText().toString().trim();
                 if(validacionesLogin(correo, password)) {
-                    /*if(correo.equals("profesor") && password.equals("profesor")){
+                    if(correo.equals("profesor@profesor.com") && password.equals("profesor")){
                         Intent cuestionariosProfesor=new Intent(LoginActivity.this, CuestionariosProfesor.class);
                         startActivity(cuestionariosProfesor);
-                    } else if (correo.equals("alumno") && password.equals("alumno")) {
+                    } else if (correo.equals("alumno@alumno.com") && password.equals("alumno")) {
                         Intent cuestionariosAlumno=new Intent(LoginActivity.this, CuestionariosAlumno.class);
                         startActivity(cuestionariosAlumno);
-                    } */
+                    } else {
+                        //TODO: enque con response tipo Object (Generico)
+                        Retrofit retrofit = new Retrofit.Builder()
+                                .baseUrl("https://serviciosocial-backend.mybluemix.net/ssfca/api/v1/")
+                                .addConverterFactory(GsonConverterFactory.create())
+                                .build();
 
-                    Retrofit retrofit = new Retrofit.Builder()
-                            .baseUrl("https://serviciosocial-backend.mybluemix.net/ssfca/api/v1/")
-                            .addConverterFactory(GsonConverterFactory.create())
-                            .build();
+                        jsonApi = retrofit.create(JsonApi.class);
 
-                    jsonApi = retrofit.create(JsonApi.class);
+                        Call login = jsonApi.loginUser(new Login(correo, password));
 
-                    Call<ResultResponse> login = jsonApi.loginUser(new Login(correo, password));
+                        login.enqueue(new Callback() {
+                            @Override
+                            public void onResponse(Call call, Response response) {
+                                System.out.println(response);
+                                System.out.println(response.body());
 
-                    login.enqueue(new Callback<ResultResponse>() {
+                            }
+
+                            @Override
+                            public void onFailure(Call call, Throwable t) {
+
+                            }
+                        });
+
+                        // TODO: Enque login con response tipo ResultResponse
+                    /*login.enqueue(new Callback<ResultResponse>() {
                         @Override
                         public void onResponse(Call<ResultResponse> call, Response<ResultResponse> response) {
                             if(!response.isSuccessful()){
                                 System.out.println("Codigo: " + response.code() + "\n");
                                 System.out.println("Mensaje: " + response.message() + "\n");
+                                System.out.println("Mensaje: " + response.body() + "\n");
 
                                 if(response.code() == 404) {
                                     alertaLoginError("Usuario no encontrado", "No se encuentra el usuario favor de intentar nuevamente, o registrarse");
@@ -106,10 +137,13 @@ public class LoginActivity extends AppCompatActivity {
                                 return;
                             }
 
-                            System.out.println("Codigo: " + response.code() + "\n");
-                            System.out.println("Mensaje: " + response.message() + "\n");
-                            token = response.message();
-                            System.out.println("Token: " + response.body().getMessage());
+                            System.out.println("Response: " + response + "\n");
+                            //token = response.message();
+                            //System.out.println("Token: " + response.body().getMessage());
+
+                            //JWT parsedJWT = new JWT(token);
+                            //System.out.println(parsedJWT.toString());
+
                             alertaLoginExitoso("Iniciando Sesión", "Por favor, espere...");
                         }
 
@@ -117,7 +151,39 @@ public class LoginActivity extends AppCompatActivity {
                         public void onFailure(Call<ResultResponse> call, Throwable t) {
 
                         }
-                    });
+                    });*/
+
+
+                        // TODO: Intento con Volley
+                    /*RequestQueue myrequest = Volley.newRequestQueue(getApplicationContext());
+                    try {
+                        JSONObject json = new JSONObject();
+                        json.put("correo", correo);
+                        json.put("contrasenia", password);
+
+                        JSONObject notification = new JSONObject();
+                        notification.put("body", json);
+
+                        json.put("notification", notification);
+
+                        String URL = "https://serviciosocial-backend.mybluemix.net/ssfca/api/v1/usuarios/login";
+
+                        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, URL, json, null, null) {
+                            @Override
+                            public Map<String, String> getHeaders() {
+                                Map<String, String> header = new HashMap<>();
+                                header.put("Content-Type", "application/json");
+                                header.put("Accept", "application/json");
+                                return header;
+                            }
+                        };
+
+                        myrequest.add(request);
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }*/
+                    }
                 }
             }
         });
